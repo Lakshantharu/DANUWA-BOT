067a49a084bd73bf2a3cae8d3b5b29febe598218
Lakshantharu <120786465+Lakshantharu@users.noreply.github.com>
2025-06-09 20:24:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "vUp2GYKA#Ku3wHuBFQR38RtEY0TS8Z57e0zNVYR6JsC71JAWLSO4", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
-  ALIVE_MSG: "*Hello👋 DANUWA-MD Is Alive Now😍*", // Change alive msg from here
+  ALIVE_MSG: "*Hello👋 Tharu-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
   GEMINI_API_KEY: "", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
